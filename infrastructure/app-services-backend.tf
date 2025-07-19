b26ddea5d4026f91f037fd2d910bd8ee8fab25ef
OMPRAKASH MISHRA <omprakashmishra3978@gmail.com>
2025-07-19 10:30:13 -0700

    fix: dependency

--- infrastructure/app-services-backend.tf
+++ infrastructure/app-services-backend.tf
@@ -5,7 +5,7 @@ resource "azurerm_service_plan" "backend" {
   location            = var.location
   os_type             = "Linux"
   sku_name            = var.app_service_sku_name_backend
-
+  depends_on          = [azurerm_resource_group.main]
 
   tags = var.common_tags
   lifecycle {
