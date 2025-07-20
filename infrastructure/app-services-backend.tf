e16aec4a90bab13862257677040a4dbecaeb4f56
OMPRAKASH MISHRA <omprakashmishra3978@gmail.com>
2025-07-20 12:18:42 -0700

    fix: caddy errors and frontdoor traffic

--- infrastructure/app-services-backend.tf
+++ infrastructure/app-services-backend.tf
@@ -75,6 +75,20 @@ resource "azurerm_linux_web_app" "backend" {
       }
     }
     ip_restriction {
+      service_tag               = "AzureFrontDoor.Backend"
+      ip_address                = null
+      virtual_network_subnet_id = null
+      action                    = "Allow"
+      priority                  = 100
+      headers {
+        x_azure_fdid      = [azurerm_cdn_frontdoor_profile.frontend_frontdoor.resource_guid]
+        x_fd_health_probe = []
+        x_forwarded_for   = []
+        x_forwarded_host  = []
+      }
+      name = "Allow traffic from Front Door"
+    }
+    ip_restriction {
       name        = "DenyAll"
       action      = "Deny"
       priority    = 500
