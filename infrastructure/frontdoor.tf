251690a182e6c8755a89c8eac2a8a3cef8c06c49
OMPRAKASH MISHRA <omprakashmishra3978@gmail.com>
2025-07-20 10:13:37 -0700

    remove compress

--- infrastructure/frontdoor.tf
+++ infrastructure/frontdoor.tf
@@ -61,51 +61,5 @@ resource "azurerm_cdn_frontdoor_route" "frontend_route" {
   https_redirect_enabled = true
   cache {
     query_string_caching_behavior = "IgnoreQueryString"
-    compression_enabled           = true
-    content_types_to_compress     = [
-      "text/html",
-      "text/javascript",
-      "text/xml",
-      "application/eot",
-      "application/font",
-      "application/font-sfnt",
-      "application/javascript",
-      "application/json",
-      "application/opentype",
-      "application/otf",
-      "application/pkcs7-mime",
-      "application/truetype",
-      "application/ttf",
-      "application/vnd.ms-fontobject",
-      "application/xhtml+xml",
-      "application/xml",
-      "application/xml+rss",
-      "application/x-font-opentype",
-      "application/x-font-truetype",
-      "application/x-font-ttf",
-      "application/x-httpd-cgi",
-      "application/x-mpegurl",
-      "application/x-opentype",
-      "application/x-otf",
-      "application/x-perl",
-      "application/x-ttf",
-      "application/x-javascript",
-      "font/eot",
-      "font/ttf",
-      "font/otf",
-      "font/opentype",
-      "image/svg+xml",
-      "text/css",
-      "text/csv",
-      "text/html",
-      "text/javascript",
-      "text/js",
-      "text/plain",
-      "text/richtext",
-      "text/tab-separated-values",
-      "text/xml",
-      "text/x-script",
-      "text/x-component"
-    ]
   }
 }
