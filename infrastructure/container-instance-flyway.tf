446e6417ec5591f813be407e17772063b38ae43f
Om Mishra <32200996+mishraomp@users.noreply.github.com>
2025-07-20 17:03:23 -0700

    Update container-instance-flyway.tf

--- infrastructure/container-instance-flyway.tf
+++ infrastructure/container-instance-flyway.tf
@@ -3,7 +3,7 @@ resource "azurerm_container_group" "flyway" {
   location            = var.location
   resource_group_name = var.resource_group_name
   subnet_ids          = [azapi_resource.container_instance_subnet.id]
-  priority            = "Spot"
+  priority            = "Regular"
   diagnostics {
     log_analytics {
       workspace_id = azurerm_log_analytics_workspace.main.workspace_id
