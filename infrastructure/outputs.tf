8a8b215e768e0bb4b4388a0054c3c141b3b3735f
OMPRAKASH MISHRA <omprakashmishra3978@gmail.com>
2025-07-22 20:27:51 -0700

    more fixes

--- infrastructure/outputs.tf
+++ infrastructure/outputs.tf
@@ -1,6 +1,6 @@
 output "cloudbeaver_app_service_url" {
   description = "The URL of the CloudBeaver App Service"
-  value       = var.enable_psql_sidecar ? "https://${module.backend.app_service.default_hostname}" : null
+  value       = var.enable_psql_sidecar ? "https://${module.backend.cloudbeaver_app_service_url}" : null
 }
 
 output "cdn_frontdoor_endpoint_url" {
