8601de3fe20a75950d8e7811adef213520f98117
OMPRAKASH MISHRA <omprakashmishra3978@gmail.com>
2025-07-22 20:31:39 -0700

    some more

--- infrastructure/outputs.tf
+++ infrastructure/outputs.tf
@@ -1,6 +1,6 @@
 output "cloudbeaver_app_service_url" {
   description = "The URL of the CloudBeaver App Service"
-  value       = var.enable_psql_sidecar ? "https://${module.backend.cloudbeaver_app_service_url}" : null
+  value       = module.backend.cloudbeaver_app_service_url != null ? "https://${module.backend.cloudbeaver_app_service_url}" : null
 }
 
 output "cdn_frontdoor_endpoint_url" {
