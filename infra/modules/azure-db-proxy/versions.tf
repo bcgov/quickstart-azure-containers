2eb2d46f57033fca2396f04c7385fd950cc846c9
OMPRAKASH MISHRA <omprakashmishra3978@gmail.com>
2026-01-01 18:50:08 -0800

    fix: version constraint

--- infra/modules/azure-db-proxy/versions.tf
+++ infra/modules/azure-db-proxy/versions.tf
@@ -5,5 +5,9 @@ terraform {
       source  = "hashicorp/azurerm"
       version = ">= 4.57.0, < 5.0.0"
     }
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 3.7.2, < 4.0.0"
+    }
   }
 }
